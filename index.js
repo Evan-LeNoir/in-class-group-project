9d7b520e1a051d721691d9418671c1a7bb331527
Charles <cbarringer6@gmail.com>
2020-06-11 20:09:47 -0500

    Saving place

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+import { nav } from "./nav.js";
+import Navigo from "navigo";
+import { Header, Nav, Main, Footer } from "./components";
+
+const router = new Navigo(window.location.origin).resolve();
+
+router
+  .on({
+    "/": () => render(nav).resolve()
+  }).resolve();
+function render(Home)
+
+
 // add menu toggle to bars icon in nav bar
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
 });
+
+
---
+++ store/Links.js
@@ -0,0 +1,7 @@
+export default [
+{ title: "", : ""},
+{ title: "", : ""},
+{ title: "", : ""},
+{ title: "", : ""},
+{ title: "", : ""},
+];
