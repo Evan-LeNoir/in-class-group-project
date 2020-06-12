5e93dc421dd28bb613e05239e599d045e821fc85
Joseph Jordan <jumpman.fam314@gmail.com>
2020-06-11 20:41:01 -0500

    saving place

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { Header, Main, Footer } from "./Components";
+
+const render = () => {
+  document.querySelector("#root").innerHTML = `
+  ${Header()}
+  ${Main()}
+  ${Footer()}
+  `;
+};
+render();
+
 // add menu toggle to bars icon in nav bar
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
