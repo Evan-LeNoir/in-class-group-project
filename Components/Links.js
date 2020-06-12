498a6c26ff411fef0aa1faf45ca353fd05b4dc2c
Brandon Bunch <brandon_bunch@icloud.com>
2020-06-11 21:08:47 -0500

    Update Links.js

--- Components/Links.js
+++ Components/Links.js
@@ -2,13 +2,13 @@ export default [
 { title: "BrandonBunch", text: "Brandon Bunch"},
 { title: "Brian", text: "Brian"},
 { title: "CharlesBarringer", text: "Charles Barringer"},
-{ title: "EvanLenoir", text: "Evan Lenoir"},
+{ title: "EvanLeNoir", text: "Evan Lenoir"},
 { title: "JasonBrandenburger", text: "Jason Brandenburger"},
 { title: "JoeSpector", text: "Joe Spector"}
 { title: "JonDove", text: "Jon Dove"}
 { title: "JosephJordan", text: "Joseph Jordan"}
-{ title: "JoshuaDeeke.js", text: "Joshua Deeke"}
-{ title: "JustinWhite.js", text: "Justin White"}
-{ title: "LaurenKye.js", text: "Lauren Kye"}
-{ title: "LillianBlair.js", text: "Lillian Blair"}
+{ title: "JoshuaDeeke", text: "Joshua Deeke"}
+{ title: "JustinWhite", text: "Justin White"}
+{ title: "LaurenKye", text: "Lauren Kye"}
+{ title: "LillianBlair", text: "Lillian Blair"}
 ];
